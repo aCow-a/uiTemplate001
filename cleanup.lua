9febf86b49bd69c523f1addfdc49ad4472e0e440
aCow-a <61611930+aCow-a@users.noreply.github.com>
2020-02-29 10:20:53 -0700

    Update cleanup.lua

--- cleanup.lua
+++ cleanup.lua
@@ -1,10 +1,14 @@
 return function()
-  for _, v in pairs(main.events) do
-    v:Disconnect()
+  if main.events and typeof(main.events) == "table" then
+    for _, v in pairs(main.events) do
+      v:Disconnect()
+    end
   end
   
-  for _, v in pairs(main.ui) do
-    v:Destroy()
+  if main.ui and typeof(main.ui) == "table" then
+    for _, v in pairs(main.ui) do
+      v:Destroy()
+    end
   end
   
   print("finished cleanup")
