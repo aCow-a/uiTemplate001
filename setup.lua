5a4cd78542e44adbbccde6ac14fa61470a9863de
aCow-a <61611930+aCow-a@users.noreply.github.com>
2020-02-29 10:38:23 -0700

    Update setup.lua

--- setup.lua
+++ setup.lua
@@ -1,7 +1,7 @@
 return function()
   main.events = {}
   main.ui = {}
-  
+  print(1)
   local playerGui = game.Players.LocalPlayer.PlayerGui
   
   local screenGui = Instance.new("ScreenGui", playerGui)
