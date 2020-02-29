ae6a9f8da42925d8e1c5aa401f6710bfee3d110a
aCow-a <61611930+aCow-a@users.noreply.github.com>
2020-02-29 10:34:58 -0700

    Update setup.lua

--- setup.lua
+++ setup.lua
@@ -6,7 +6,7 @@ return function()
   table.insert(main.ui, screenGui)
   
   local frame = Instance.new("Frame", screenGui)
-  frame.Size = Udim2.new(0, 200, 0, 300)
+  --frame.Size = Udim2.new(0, 200, 0, 300)
   
   print("finished setup")
 end
