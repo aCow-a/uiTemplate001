95adace464567bbe607ba23c735aeed07b01f246
aCow-a <61611930+aCow-a@users.noreply.github.com>
2020-02-29 10:41:08 -0700

    Update setup.lua

--- setup.lua
+++ setup.lua
@@ -1,13 +1,14 @@
 return function()
   main.events = {}
   main.ui = {}
-  print(1)
+  
   local playerGui = game.Players.LocalPlayer.PlayerGui
   
-  local screenGui = Instance.new("ScreenGui", playerGui)
+  screenGui = Instance.new("ScreenGui", playerGui)
   table.insert(main.ui, screenGui)
   
-
+  frame = Instance.new("Frame", screenGui)
+  frame.Size = UDim2.new(0, 200, 0, 450)
   
   print("finished setup")
 end
