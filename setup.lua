3195125bbc2c9c84a0576401648d6e76431f0f49
aCow-a <61611930+aCow-a@users.noreply.github.com>
2020-02-29 10:17:41 -0700

    Update setup.lua

--- setup.lua
+++ setup.lua
@@ -2,7 +2,17 @@ return function()
   main.events = {}
   main.ui = {}
   
+  local screenGui = Instance.new("ScreenGui", playerGui)
+  table.insert(main.ui, screenGui)
+  
+  local frame = Instance.new("Frame", screenGui)
+  frame.Size = Vector3.new(0, 200, 0, 300)
+  
+  
+  
+  
   local part = Instance.new("Part", workspace)
   table.insert(main.ui, part)
-  print("test")
+  
+  print("finished setup")
 end
